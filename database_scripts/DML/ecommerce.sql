2a81314f9444e909ecbb998cb5ef6c1018962472
Darky1948 <kristen.viguier@gmail.com>
2017-10-14 15:39:15 +0200

    Real things income
    Education is important but big biceps are importanter ! 
    Don't forget to listen https://open.spotify.com/user/1139696773/playlist/4PjjtuJOJApL4NLmzj2Pln when you do a program !

--- database_scripts/DML/ecommerce.sql
+++ database_scripts/DML/ecommerce.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Ven 13 Octobre 2017 à 13:59
+-- Généré le :  Sam 14 Octobre 2017 à 13:38
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -36,10 +36,6 @@ CREATE TABLE `admin` (
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- RELATIONS POUR LA TABLE `admin`:
---
-
 -- --------------------------------------------------------
 
 --
@@ -60,10 +56,6 @@ CREATE TABLE `article` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- RELATIONS POUR LA TABLE `article`:
---
-
---
 -- Contenu de la table `article`
 --
 
@@ -71,7 +63,43 @@ INSERT INTO `article` (`idArticle`, `libelle`, `price`, `quantity`, `article_siz
 (1, 'WROGN Men\'s Purple Printed Polo T-Shirt', '979', 5, 'L', 'Purple', '2017-10-13 13:56:00', 1, 2, 1),
 (2, 'DUKE Multi Color Striped Polo T-shirt', '795', 5, 'L', 'Multicolor', '2017-10-13 13:56:01', 1, 1, 2),
 (3, 'DNMX Gray & Black Striped Polo T-shirt', '699', 5, 'L', 'Multicolor', '2017-10-13 13:56:01', 1, 4, 3),
-(4, 'Celio Navy & Green Slim Fit Polo T-shirt', '950', 5, 'L', 'Navy blue', '2017-10-13 13:56:01', 1, 3, 4);
+(4, 'Celio Navy & Green Slim Fit Polo T-shirt', '950', 5, 'L', 'Navy blue', '2017-10-13 13:56:01', 1, 3, 4),
+(5, 'Fighting Fame Printed Denim Henley T-Shirt', '999', 5, 'L', 'Navy blue', '2017-10-14 13:17:09', 2, 1, 5),
+(6, 'Yukth Solid Mens Henley Green T-Shirt', '950', 5, 'L', 'Navy blue', '2017-10-14 13:17:09', 2, 6, 6),
+(7, 'Bi fashion Henley maroon Half sleeve T-shirt', '399', 5, 'L', 'Maroon', '2017-10-14 13:17:09', 2, 7, 8),
+(8, 'Metronaut Striped Men\'s Henley White T-Shirt', '404', 5, 'L', 'Half Sleeve', '2017-10-14 13:17:09', 2, 8, 9),
+(9, 'Symbol Yellow Cotton V-Neck Men\'s T-Shirt', '999', 5, 'L', 'Yellow', '2017-10-14 13:20:50', 3, 7, 10),
+(10, 'Seven Rocks Men\'s Black V-Neck Cotton Tshirt', '999', 5, 'L', 'Black', '2017-10-14 13:20:50', 3, 7, 11),
+(11, 'Jockey Men\'s Black Cotton V-Neck T-Shirt', '999', 5, 'L', 'Black', '2017-10-14 13:20:50', 3, 7, 12),
+(12, 'ZCELL Black Cotton Solid Slim Fit Casual T-Shirts', '999', 5, 'L', 'Black Gray', '2017-10-14 13:20:50', 3, 7, 13),
+(13, 'Spykar Navy Blue Striped Regular Fit Round Neck T-Shirt', '990', 5, 'L', 'Navy blue', '2017-10-14 13:23:29', 4, 7, 13),
+(14, 'Gritstones Brown Striped Round Neck T-Shirt', '990', 5, 'L', 'Multicolor', '2017-10-14 13:23:29', 4, 7, 14),
+(15, 'Rigo Rust Striped Regular Fit Round Neck T-Shirt', '450', 5, 'L', 'Rust', '2017-10-14 13:23:30', 4, 7, 15),
+(16, 'Cult Fiction Green Striped Regular Fit Round Neck T-Shirt', '518', 5, 'L', 'Grey green', '2017-10-14 13:23:30', 4, 7, 16),
+(17, 'AJIO Graphic Print T-shirt', '200', 5, 'L', 'Grey green', '2017-10-14 13:26:13', 5, 7, 17),
+(18, 'Mexx Black Printed Regular Fit Round Neck T-Shirt', '242', 5, 'L', 'Black', '2017-10-14 13:26:13', 5, 7, 18),
+(19, 'AJIO Graphic Print T-shirt', '195', 5, 'L', 'Black', '2017-10-14 13:26:13', 5, 7, 19),
+(20, 'AJIO Printed Crew-Neck T-shirt', '230', 5, 'L', 'Multicolor', '2017-10-14 13:26:13', 5, 7, 20),
+(21, 'Spring Break Men\'s Black & White Color Block T-Shirt With Patch Pocket', '499', 5, 'L', 'Black White', '2017-10-14 13:28:49', 6, 5, 21),
+(22, 'Mast & Harbour White Striped V-Neck Pocket Tee', '499', 5, 'L', 'White', '2017-10-14 13:28:49', 6, 2, 22),
+(23, 'Indigo Nation Maroon Printed Round Neck Pocket Tee', '499', 5, 'L', 'Maroon', '2017-10-14 13:28:49', 6, 1, 23),
+(24, 'HIGHLANDER colour block cotton t-shirt', '274', 5, 'L', 'Multicolor', '2017-10-14 13:28:49', 6, 3, 24),
+(25, 'Kindred Extended Hem T-Shirt With Suspender', '499', 5, 'L', 'Black', '2017-10-14 13:31:11', 7, 8, 25),
+(26, 'Garcon Peach & Blue Solid Crew Neck T-Shirt', '899', 5, 'L', 'Black', '2017-10-14 13:31:11', 7, 7, 26),
+(27, 'Funky Guys Solid Men\'s Round Neck Yellow T-Shirt', '177', 5, 'L', 'Yellow', '2017-10-14 13:31:11', 7, 7, 27),
+(28, 'Blotch Red Cotton Striped Sleeve With Zip T-shirt', '177', 5, 'L', 'Red', '2017-10-14 13:31:11', 7, 6, 28),
+(29, 'KOOVS Back Print Hooded T-Shirt', '799', 5, 'L', 'Grey', '2017-10-14 13:33:11', 8, 1, 29),
+(30, 'KOOVS Olive Back Printed Hooded T-Shirt', '799', 5, 'L', 'Olive', '2017-10-14 13:33:11', 8, 2, 30),
+(31, 'KOOVS Slogan Print Double Sleeve Hooded Twofer', '999', 5, 'L', 'Black', '2017-10-14 13:33:11', 8, 3, 31),
+(32, 'FOREVER 21 Men Pink Solid Hooded T-shirt', '949', 5, 'L', 'Pink', '2017-10-14 13:33:11', 8, 4, 32),
+(33, 'AJIO Flock Print Crew-Neck T-shirt', '200', 5, 'L', 'MultiColor', '2017-10-14 13:35:33', 9, 4, 33),
+(34, 'AJIO Printed Crew-Neck T-shirt', '160', 5, 'L', 'Yellow', '2017-10-14 13:35:34', 9, 5, 34),
+(35, 'AJIO Typographic Print Crew-Neck T-shirt', '200', 5, 'L', 'Blue', '2017-10-14 13:35:34', 9, 6, 35),
+(36, 'AJIO Printed Crew-Neck T-shirt', '200', 5, 'L', 'Grey', '2017-10-14 13:35:34', 9, 7, 36),
+(37, 'KOOVS Extreme Scoop Neck T-Shirt', '399', 5, 'L', 'Green', '2017-10-14 13:37:25', 10, 8, 37),
+(38, 'KOOVS Scoop Neck T-Shirt', '395', 5, 'L', 'Black', '2017-10-14 13:37:25', 10, 1, 38),
+(39, 'Blue Saint Exclusive Scoop Neck T-Shirt', '399', 5, 'L', 'Blue Saint', '2017-10-14 13:37:25', 10, 2, 39),
+(40, 'KOOVS Extreme Scoop Neck T-Shirt', '399', 5, 'L', 'Kind of white', '2017-10-14 13:37:26', 10, 3, 40);
 
 -- --------------------------------------------------------
 
@@ -88,19 +116,13 @@ CREATE TABLE `articletype` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- RELATIONS POUR LA TABLE `articletype`:
---   `idCategory`
---       `category` -> `idCategory`
---
-
---
 -- Contenu de la table `articletype`
 --
 
 INSERT INTO `articletype` (`idType`, `libelle`, `valid`, `reg_date`, `idCategory`) VALUES
 (1, 'POLO', 1, '2017-10-13 11:37:43', 7),
 (2, 'HENLEY', 1, '2017-10-13 11:37:43', 7),
-(3, 'V-NECk', 1, '2017-10-13 11:37:43', 7),
+(3, 'V-NECK', 1, '2017-10-14 13:38:43', 7),
 (4, 'STRIPED', 1, '2017-10-13 11:37:43', 7),
 (5, 'GRAPHIC', 1, '2017-10-13 11:37:43', 7),
 (6, 'POCKET', 1, '2017-10-13 11:37:43', 7),
@@ -122,10 +144,6 @@ CREATE TABLE `brand` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- RELATIONS POUR LA TABLE `brand`:
---
-
---
 -- Contenu de la table `brand`
 --
 
@@ -133,7 +151,11 @@ INSERT INTO `brand` (`idBrand`, `brand_name`, `reg_date`) VALUES
 (1, 'DUKE', '2017-10-13 13:49:56'),
 (2, 'WROGN', '2017-10-13 13:49:56'),
 (3, 'CELIO', '2017-10-13 13:49:56'),
-(4, 'DNMX', '2017-10-13 13:50:19');
+(4, 'DNMX', '2017-10-13 13:50:19'),
+(5, 'DENIM', '2017-10-14 13:08:10'),
+(6, 'Yukth', '2017-10-14 13:08:10'),
+(7, 'Unknown', '2017-10-14 13:08:10'),
+(8, 'Metronaut', '2017-10-14 13:08:10');
 
 -- --------------------------------------------------------
 
@@ -146,14 +168,6 @@ CREATE TABLE `cart` (
   `idArticle` int(6) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- RELATIONS POUR LA TABLE `cart`:
---   `idArticle`
---       `article` -> `idArticle`
---   `idCustomer`
---       `customer` -> `idCustomer`
---
-
 -- --------------------------------------------------------
 
 --
@@ -165,10 +179,6 @@ CREATE TABLE `category` (
   `libelle` varchar(30) NOT NULL,
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELATIONS POUR LA TABLE `category`:
---
 
 --
 -- Contenu de la table `category`
@@ -193,14 +203,6 @@ CREATE TABLE `comment` (
   `idCustomer` int(6) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- RELATIONS POUR LA TABLE `comment`:
---   `idArticle`
---       `article` -> `idArticle`
---   `idCustomer`
---       `customer` -> `idCustomer`
---
-
 -- --------------------------------------------------------
 
 --
@@ -221,10 +223,6 @@ CREATE TABLE `customer` (
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- RELATIONS POUR LA TABLE `customer`:
---
-
 -- --------------------------------------------------------
 
 --
@@ -237,10 +235,6 @@ CREATE TABLE `media` (
   `file_name` varchar(200) DEFAULT NULL,
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELATIONS POUR LA TABLE `media`:
---
 
 --
 -- Contenu de la table `media`
@@ -301,12 +295,6 @@ CREATE TABLE `orders` (
   `idCustomer` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- RELATIONS POUR LA TABLE `orders`:
---   `idCustomer`
---       `customer` -> `idCustomer`
---
-
 -- --------------------------------------------------------
 
 --
@@ -320,14 +308,6 @@ CREATE TABLE `recommandedarticle` (
   `percentage` decimal(10,0) DEFAULT NULL,
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- RELATIONS POUR LA TABLE `recommandedarticle`:
---   `idArticle1`
---       `article` -> `idArticle`
---   `idArticle2`
---       `article` -> `idArticle`
---
 
 --
 -- Index pour les tables exportées
@@ -419,7 +399,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `idArticle` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idArticle` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 --
 -- AUTO_INCREMENT pour la table `articletype`
 --
@@ -429,7 +409,7 @@ ALTER TABLE `articletype`
 -- AUTO_INCREMENT pour la table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `idBrand` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idBrand` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `category`
 --
--- database_scripts/DML/whereamitofilldattabase.sql
+++ database_scripts/DML/whereamitofilldattabase.sql
@@ -1,9 +1,53 @@
 -- polo 1 fait 
 -- https://www.looksgud.in/blog/different-types-of-t-shirts-men/#Polo T-Shirt
   
-  INSERT INTO `article`
+INSERT INTO `article`
 (`libelle`, `price`, `quantity`, `article_size`, `color`, `reg_date`,
- `idType`, `idBrand`, `idMedia`) 
- VALUES 
- ('Celio Navy & Green Slim Fit Polo T-shirt',950,5,'L','Navy blue', CURRENT_TIMESTAMP,
+`idType`, `idBrand`, `idMedia`) 
+VALUES 
+('Celio Navy & Green Slim Fit Polo T-shirt',950,5,'L','Navy blue', CURRENT_TIMESTAMP,
  1,3,4);
+ 
+INSERT INTO `brand`(`brand_name`, `reg_date`) VALUES ('DENIM',CURRENT_TIMESTAMP);
+INSERT INTO `brand`(`brand_name`, `reg_date`) VALUES ('Yukth',CURRENT_TIMESTAMP);
+INSERT INTO `brand`(`brand_name`, `reg_date`) VALUES ('Unknown',CURRENT_TIMESTAMP);
+INSERT INTO `brand`(`brand_name`, `reg_date`) VALUES ('Metronaut',CURRENT_TIMESTAMP);
+ 
+ 
+/*
+articleType
+id type libelle
+						1		DUKE
+						2		WROGN
+						3		CELIO
+						4		DNMX
+						5		DENIM
+						6		Yukth
+						7		Unknown
+						8		Metronaut
+10		U NECK/SCOOP NECK
+*/
+INSERT INTO `article`
+(`libelle`, `price`, `quantity`, `article_size`, `color`, `reg_date`,
+`idType`, `idBrand`, `idMedia`) 
+VALUES 
+('KOOVS Extreme Scoop Neck T-Shirt',399,5,'L','Green', CURRENT_TIMESTAMP,
+ 10,8,37);
+INSERT INTO `article`
+(`libelle`, `price`, `quantity`, `article_size`, `color`, `reg_date`,
+`idType`, `idBrand`, `idMedia`) 
+VALUES 
+('KOOVS Scoop Neck T-Shirt',395,5,'L','Black', CURRENT_TIMESTAMP,
+ 10,1,38);
+INSERT INTO `article`
+(`libelle`, `price`, `quantity`, `article_size`, `color`, `reg_date`,
+`idType`, `idBrand`, `idMedia`) 
+VALUES 
+('Blue Saint Exclusive Scoop Neck T-Shirt',399,5,'L','Blue Saint', CURRENT_TIMESTAMP,
+ 10,2,39);
+ INSERT INTO `article`
+(`libelle`, `price`, `quantity`, `article_size`, `color`, `reg_date`,
+`idType`, `idBrand`, `idMedia`) 
+VALUES 
+('KOOVS Extreme Scoop Neck T-Shirt',399,5,'L','Kind of white', CURRENT_TIMESTAMP,
+ 10,3,40);
